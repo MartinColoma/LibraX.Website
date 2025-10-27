f74332653f9d29703c81579564815a47fde0e18b
Earlypearly <earljoshualiporada@gmail.com>
2025-10-27 16:18:38 +0800

    add vercel json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/" || "/LibraX.Website",
-
+  base: "/",
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 })
